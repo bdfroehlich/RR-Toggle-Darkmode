9eab2d80a6563c17b773053407667aa4b06c97a1
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-04-09 13:18:59 -0500

    initial commit, project structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
+import { darkModeOn, lightModeOn } from './features/modeSlice'
 
 function App() {
+
+  const dispatch = useDispatch()
+  const mode = useSelector(state => state.mode);
+
+  const toggleMode = () => {
+    mode.darkMode ? dispatch(lightModeOn()) : dispatch(darkModeOn())
+  }
   
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
+    <div style={{ backgroundColor: mode.color1, color: 'black' }} className="App">
       <Nav />
+      <button onClick={toggleMode}>{ mode.darkMode ? 'Light Mode' : 'Dark Mode' }</button>
       <ContentWrapper />
       <Footer />
     </div>
